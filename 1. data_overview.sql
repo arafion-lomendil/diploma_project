5c4949b76b466f7f5e7e35e7af6a1450c38de453
Денис <drubtsov90@gmail.com>
2026-09-17 16:43:05 +0300

    Update 1. data_overview.sql

--- 1. data_overview.sql
+++ 1. data_overview.sql
@@ -2,24 +2,29 @@
 SELECT COUNT(order_id) AS order_cnt,
 COUNT(DISTINCT user_id) AS user_cnt
 FROM purchases;
+
 SELECT MIN(created_dt_msk)::date AS min_date,
 MAX(created_dt_msk)::date AS max_date
 FROM purchases;
+
 SELECT age_limit,
 COUNT(order_id) AS order_cnt 
 FROM purchases
 GROUP BY 1
 ORDER BY 2 DESC;
+
 SELECT device_type_canonical,
 COUNT(order_id) AS order_cnt
 FROM purchases
 GROUP BY 1
 ORDER BY 2 DESC;
+
 SELECT currency_code,
 COUNT(order_id) AS order_cnt
 FROM purchases
 GROUP BY 1
 ORDER BY 2 DESC;
+
 SELECT MIN(revenue) AS min_revenue,
 MAX(revenue) AS max_revenue,
 ROUND(AVG(revenue),4) AS avg_revenue,
