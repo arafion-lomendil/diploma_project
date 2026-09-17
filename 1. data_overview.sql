f98ab4d50f466aa853cd4b20dec1de473da9a286
Денис <drubtsov90@gmail.com>
2026-09-17 14:19:34 +0300

    Create 1. data_overview.sql

---
+++ 1. data_overview.sql
@@ -0,0 +1,27 @@
+--Знакомство с данными таблицы purchases
+SELECT COUNT(order_id) AS order_cnt,
+COUNT(DISTINCT user_id) AS user_cnt
+FROM purchases;
+SELECT MIN(created_dt_msk)::date AS min_date,
+MAX(created_dt_msk)::date AS max_date
+FROM purchases;
+SELECT age_limit,
+COUNT(order_id) AS order_cnt 
+FROM purchases
+GROUP BY 1
+ORDER BY 2 DESC;
+SELECT device_type_canonical,
+COUNT(order_id) AS order_cnt
+FROM purchases
+GROUP BY 1
+ORDER BY 2 DESC;
+SELECT currency_code,
+COUNT(order_id) AS order_cnt
+FROM purchases
+GROUP BY 1
+ORDER BY 2 DESC;
+SELECT MIN(revenue) AS min_revenue,
+MAX(revenue) AS max_revenue,
+ROUND(AVG(revenue),4) AS avg_revenue,
+STDDEV(revenue) AS std_revenue
+FROM purchases;
